ad9b9cb97170545ae5f6e920e8722cf90f3d5502
Milana <mukanmilana@gmail.com>
2024-03-21 15:55:42 +0200

    Add project files.

---
+++ Client/Client.cpp
@@ -0,0 +1,155 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+// #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <vector>
+#include <string>
+using namespace std;
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+#define PAUSE 1000
+
+
+int main(int argc, char** argv) // имя сервера при желании можно будет указать через параметры командной строки
+{
+
+    setlocale(0, "");
+    system("title CLIENT SIDE");
+    cout << "процесс клиента запущен!\n";
+
+    
+    // Initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        cout << "WSAStartup failed with error: "<<iResult << "\n";
+        return 11;
+    }
+    else {
+        cout << "подключение Winsock.dll прошло успешно!\n";
+    }
+
+
+    addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    const char* ip = "localhost";
+
+    addrinfo* result = NULL;
+    iResult = getaddrinfo(ip, DEFAULT_PORT, &hints, &result);
+
+    if (iResult != 0) {
+        cout << "getaddrinfo failed with error: " << iResult << "\n";
+        WSACleanup();
+        return 12;
+    }
+    else {
+        cout << "получение адреса и порта клиента прошло успешно!\n";
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next) { // серверов может быть несколько, поэтому не помешает цикл
+
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+        if (ConnectSocket == INVALID_SOCKET) {
+            cout << "socket failed with error: " << WSAGetLastError() << "\n";
+            WSACleanup();
+            return 13;
+        }
+
+        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+
+        cout << "создание сокета на клиенте прошло успешно!\n";
+
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        cout << "невозможно подключиться к серверу. убедитесь, что процесс сервера запущен!\n";
+        WSACleanup();
+        return 14;
+    }
+    else {
+        cout << "подключение к серверу прошло успешно!\n";
+    }
+
+    vector<const char*> client_questions;
+
+    client_questions.push_back("Whats the weather like today?");
+    client_questions.push_back("How much does delivery to my house cost?");
+    client_questions.push_back("What is the warranty period for this item?");
+    client_questions.push_back("What promotions are currently active?");
+    client_questions.push_back("Can I order takeout?");
+    client_questions.push_back("Where is the nearest branch of your company located?");
+    client_questions.push_back("What payment methods do you accept?");
+    client_questions.push_back("How long will the delivery take?");
+    client_questions.push_back("Do you have loyalty programs for regular customers?");
+    client_questions.push_back("What services do you offer for new customers?");
+
+    cout << "Вопросы клиента:" << endl;
+    for (const auto& question : client_questions) {
+        cout << question << endl;
+    }
+    while (true) {
+        char message[DEFAULT_BUFLEN];
+        cout << "Введите ваш вопрос ('end' для завершения): ";
+        cin.getline(message, DEFAULT_BUFLEN);
+
+        if (strcmp(message, "end") == 0) {
+            cout << "Завершение работы по запросу пользователя.\n";
+            break;
+        }
+
+        iResult = send(ConnectSocket, message, (int)strlen(message), 0);
+        if (iResult == SOCKET_ERROR) {
+            cout << "send failed with error: " << WSAGetLastError() << "\n";
+            closesocket(ConnectSocket);
+            WSACleanup();
+            return 15;
+        }
+
+        char answer[DEFAULT_BUFLEN];
+
+        iResult = recv(ConnectSocket, answer, DEFAULT_BUFLEN, 0);
+        if (iResult > 0) {
+            answer[iResult] = '\0';
+            cout << "Ответ от сервера: " << answer << "\n";
+            cout << "байтов получено: " << iResult << "\n";
+        }
+        else if (iResult == 0)
+            cout << "соединение с сервером закрыто.\n";
+        else
+            cout << "recv failed with error: " << WSAGetLastError() << "\n";
+
+    }
+
+
+
+    // cleanup
+    closesocket(ConnectSocket);
+    WSACleanup();
+
+    cout << "процесс клиента прекращает свою работу!\n";
+
+    return 0;
+}
---
+++ seti1/Server.cpp
@@ -0,0 +1,199 @@
+﻿#define WIN32_LEAN_AND_MEAN // To speed up the build process: https://stackoverflow.com/questions/11040133/what-does-defining-win32-lean-and-mean-exclude-exactly
+
+#include <iostream>
+#include <windows.h>
+#include <map>
+#include <string>
+// #include <winsock2.h>
+#include <ws2tcpip.h> // WSADATA type; WSAStartup, WSACleanup functions and many more
+using namespace std;
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015" // a port is a logical construct that identifies a specific process or a type of network service - https://en.wikipedia.org/wiki/Port_(computer_networking)
+
+#define PAUSE 1000
+
+int main()
+{
+    map<const char*, const char*> question_answer_map;
+
+    question_answer_map["Whats the weather like today?"] = "The weather today is sunny, with a temperature around 25 degrees Celsius.";
+    question_answer_map["How much does delivery to my house cost?"] = "The cost of delivery depends on your location, please specify the address.";
+    question_answer_map["What is the warranty period for this item?"] = "This item comes with a 1-year warranty.";
+    question_answer_map["What promotions are currently active?"] = "We currently have the '20% Discount on the Second Item' promotion.";
+    question_answer_map["Can I order takeout?"] = "Certainly, you can order takeout, our menu is available on the website.";
+    question_answer_map["Where is the nearest branch of your company located?"] = "The nearest branch is located at: Lenin Street, No. 15.";
+    question_answer_map["What payment methods do you accept?"] = "We accept payment in cash, by bank transfer, and through payment systems.";
+    question_answer_map["How long will the delivery take?"] = "Delivery usually takes between 1 to 3 working days.";
+    question_answer_map["Do you have loyalty programs for regular customers?"] = "Yes, we have a loyalty program that offers discounts and bonuses.";
+    question_answer_map["What services do you offer for new customers?"] = "For new customers, we offer a free consultation and a discount on the first purchase.";
+
+    setlocale(0, "");
+    system("title SERVER SIDE");
+    cout << "процесс сервера запущен!\n";
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Initialize Winsock
+    WSADATA wsaData; // The WSADATA structure contains information about the Windows Sockets implementation: https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // The WSAStartup function initiates use of the Winsock DLL by a process: https://firststeps.ru/mfc/net/socket/r.php?2
+    if (iResult != 0) {
+        cout << "WSAStartup failed with error: " << iResult << "\n";
+        cout << "подключение Winsock.dll прошло с ошибкой!\n";
+        return 1;
+    }
+    else {
+        cout << "подключение Winsock.dll прошло успешно!\n";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    struct addrinfo hints; // The addrinfo structure is used by the getaddrinfo function to hold host address information: https://docs.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-addrinfoa
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET; // The Internet Protocol version 4 (IPv4) address family
+    hints.ai_socktype = SOCK_STREAM; // Provides sequenced, reliable, two-way, connection-based byte streams with a data transmission mechanism
+    hints.ai_protocol = IPPROTO_TCP; // The Transmission Control Protocol (TCP). This is a possible value when the ai_family member is AF_INET or AF_INET6
+    hints.ai_flags = AI_PASSIVE; // The socket address will be used in a call to the "bind" function
+
+    // Resolve the server address and port
+    struct addrinfo* result = NULL;
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        cout << "getaddrinfo failed with error: " << iResult << "\n";
+        cout << "получение адреса и порта сервера прошло c ошибкой!\n";
+        WSACleanup(); // The WSACleanup function terminates use of the Winsock 2 DLL (Ws2_32.dll): https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsacleanup
+        return 2;
+    }
+    else {
+        cout << "получение адреса и порта сервера прошло успешно!\n";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Create a SOCKET for connecting to server
+    SOCKET ListenSocket = INVALID_SOCKET;
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        cout << "socket failed with error: " << WSAGetLastError() << "\n";
+        cout << "создание сокета прошло c ошибкой!\n";
+        freeaddrinfo(result);
+        WSACleanup();
+        return 3;
+    }
+    else {
+        cout << "создание сокета на сервере прошло успешно!\n";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Setup the TCP listening socket
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); // The bind function associates a local address with a socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-bind
+    if (iResult == SOCKET_ERROR) {
+        cout << "bind failed with error: " << WSAGetLastError() << "\n";
+        cout << "внедрение сокета по IP-адресу прошло с ошибкой!\n";
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 4;
+    }
+    else {
+        cout << "внедрение сокета по IP-адресу прошло успешно!\n";
+    }
+
+    freeaddrinfo(result);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    iResult = listen(ListenSocket, SOMAXCONN); // The listen function places a socket in a state in which it is listening for an incoming connection: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-listen
+    if (iResult == SOCKET_ERROR) {
+        cout << "listen failed with error: " << WSAGetLastError() << "\n";
+        cout << "прослушка информации от клиента не началась. что-то пошло не так!\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 5;
+    }
+    else {
+        cout << "начинается прослушка информации от клиента. пожалуйста, запустите клиентское приложение! (client.exe)\n";
+        // здесь можно было бы запустить некий прелоадер в отдельном потоке
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Accept a client socket
+    SOCKET ClientSocket = INVALID_SOCKET;
+    ClientSocket = accept(ListenSocket, NULL, NULL); // The accept function permits an incoming connection attempt on a socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-accept
+    if (ClientSocket == INVALID_SOCKET) {
+        cout << "accept failed with error: " << WSAGetLastError() << "\n";
+        cout << "соединение с клиентским приложением не установлено! печаль!\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 6;
+    }
+    else {
+        cout << "соединение с клиентским приложением установлено успешно!\n";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // No longer need server socket
+    closesocket(ListenSocket);
+
+    // Receive until the peer shuts down the connection
+    while(true){
+
+        char message[DEFAULT_BUFLEN];
+
+        iResult = recv(ClientSocket, message, DEFAULT_BUFLEN, 0); // The recv function is used to read incoming data: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-recv
+        if (iResult > 0) {
+            message[iResult] = '\0';
+            cout << "процесс клиента прислал сообщение: " << message << "\n";
+            cout << "байтов получено: " << iResult << "\n";
+
+            bool isFound = false;
+
+            for (auto it = question_answer_map.begin(); it != question_answer_map.end(); ++it) {
+                if (strcmp(message, it->first) == 0) { 
+                    isFound = true;
+                    cout << it->second << endl;
+                    int iSendResult = send(ClientSocket, it->second, strlen(it->second), 0);
+
+                    if (iSendResult == SOCKET_ERROR) {
+                        cout << "send failed with error: " << WSAGetLastError() << "\n";
+                        cout << "упс, отправка (send) ответного сообщения не состоялась ((\n";
+                        closesocket(ClientSocket);
+                        WSACleanup();
+                        return 7;
+                    }
+                    else {
+                        cout << "байтов отправлено: " << iSendResult << "\n";
+                    }
+                }
+                if (isFound)
+                    break;
+            }
+
+        }
+        else if (iResult == 0) {
+            cout << "соединение закрывается...\n";
+        }
+        else {
+            cout << "recv failed with error: " << WSAGetLastError() << "\n";
+            cout << "упс, получение (recv) ответного сообщения не состоялось ((\n";
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 8;
+        }
+
+        }
+
+    // cleanup
+    closesocket(ClientSocket);
+    WSACleanup();
+
+    return 0;
+}
+
